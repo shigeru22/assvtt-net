65288856d54876598bce3dcd54de06e789cdcc8d
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2025-07-28 20:29:39 +0700

    foundation: Implement overridden conversion

--- Kyutorius.AstonishedVendetta.Foundation/VttConverter.cs
+++ Kyutorius.AstonishedVendetta.Foundation/VttConverter.cs
@@ -5,14 +5,18 @@
 // https://github.com/jamiees2/ass-to-vtt/blob/3b9a3c7819edb769bc30f0aa48f8a181bee37018/index.js
 // (c) jamiees2, licensed under MIT License
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Kyutorius.AstonishedVendetta.Foundation;
 
 /// <summary>
-/// Main class to convert ASS to VTT.
+/// Main ASS to VTT converter class.
 /// </summary>
 public static class VttConverter
 {
@@ -38,6 +42,11 @@ public static class VttConverter
     /// </summary>
     private static readonly Regex REGEX_STYLE = new Regex(@"\{([^}]+)\}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
+    private static readonly Regex REGEX_STYLING_TAG = new Regex(@"[a-zA-Z]+", RegexOptions.Compiled);
+    private static readonly Regex REGEX_B_TAG = new Regex(@"b(\d{3})", RegexOptions.Compiled);
+
+    private static readonly string[] TEXT_DECORATION_TAGS = ["b", "i", "u", "s"];
+
     /// <summary>
     /// Delegate type for callback on each line converted.
     /// </summary>
@@ -51,8 +60,13 @@ public static class VttConverter
     /// <param name="disableStyles">Whether to disable styles embedded from the ASS subtitle.</param>
     /// <param name="lineNumber">Line number before timestamp. Set as null to disable.</param>
     /// <returns>Converted line. May be null if error.</returns>
-    public static string? ConvertLine(string input, bool disableStyles = false, int? lineNumber = null)
+    public static string? ConvertLine(string? input, bool disableStyles = false, int? lineNumber = null)
     {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return null;
+        }
+
         // test for matching subtitle pattern
 
         Match capturedSubtitle = REGEX_ASS_GROUPING.Match(input);
@@ -107,18 +121,18 @@ public static class VttConverter
         // process style information, since subtitles may contain any number of
         // tags, loop each of them if any
 
-        string subStyle = string.Empty;
-        string subPositionStyle = string.Empty;
+        string subStyle = string.Empty; // TODO: unused?
+        StringBuilder subPositionStyle = new StringBuilder();
         Stack<string> subTagsToClose = new Stack<string>();
 
         Match capturedStyle = REGEX_STYLE.Match(lineText);
         while (capturedStyle.Success)
         {
-            string[] tagsToOpen = [];
-            string styleReplacement = string.Empty;
+            List<string> subTagsToOpen = [];
+            StringBuilder styleReplacement = new StringBuilder();
 
             // check for empty tags
-            if (capturedStyle.Groups.Count > 1 && string.IsNullOrWhiteSpace(capturedStyle.Groups[1].Value))
+            if (capturedStyle.Groups.Count > 1 && !string.IsNullOrWhiteSpace(capturedStyle.Groups[1].Value))
             {
                 // split override commands
 
@@ -126,12 +140,166 @@ public static class VttConverter
                     .Value
                     .Split(@"\");
 
-                // TODO: continue styled processing
+                // for each commands, extract tag name, and check its attribute
+
+                int currentStylesLength = currentStyles.Length;
+                for (int currentStylesIndex = 1; currentStylesIndex < currentStylesLength; currentStylesIndex++)
+                {
+                    string currentStyle = currentStyles[currentStylesIndex];
+
+                    Match capturedTag = REGEX_STYLING_TAG.Match(currentStyle);
+                    if (capturedTag.Success && !string.IsNullOrWhiteSpace(capturedTag.Groups[0].Value))
+                    {
+                        string currentTag = capturedTag.Groups[0].Value;
+
+                        // TODO: handle AsSpan() out of range exceptions
+                        if (string.Equals(currentTag, "an") && int.TryParse(currentStyle.AsSpan(2, 3), out int targetNewPosition))
+                        {
+                            // new position commands, assume bottom center
+                            // position as the default
+
+                            double lineCalculation = Math.Floor((double)(targetNewPosition - 1) / 3);
+                            int alignmentCalculation = targetNewPosition % 3;
+
+                            if (lineCalculation == 1.0)
+                            {
+                                subPositionStyle.Append(" line:50%");
+                            }
+                            else if (lineCalculation == 2.0)
+                            {
+                                subPositionStyle.Append(" line:0");
+                            }
+
+                            if (alignmentCalculation == 1)
+                            {
+                                subPositionStyle.Append(" align:start");
+                            }
+                            else if (alignmentCalculation == 2)
+                            {
+                                subPositionStyle.Append(" align:end");
+                            }
+                        }
+                        else if (string.Equals(currentTag, "a") && int.TryParse(currentStyle.AsSpan(1, 2), out int targetLegacyPosition))
+                        {
+                            // legacy position command
+
+                            int alignmentCalculation = (targetLegacyPosition - 1) % 4;
+
+                            if (targetLegacyPosition > 8)
+                            {
+                                subPositionStyle.Append(" line:50%");
+                            }
+                            else if (targetLegacyPosition > 4)
+                            {
+                                subPositionStyle.Append(" line:0");
+                            }
+
+                            if (alignmentCalculation == 0)
+                            {
+                                subPositionStyle.Append(" align:start");
+                            }
+                            else if (alignmentCalculation == 2)
+                            {
+                                subPositionStyle.Append(" align:end");
+                            }
+                        }
+                        else if (currentTag.Length == 1)
+                        {
+                            if (TEXT_DECORATION_TAGS.Contains(currentTag))
+                            {
+                                // check simple text decoration commands to be
+                                // mapped into its WebVTEGEquivalents
+                                // note that strikethrough (\s) is not supported
+                                // in WebVTT
+                                // also, b-tag should be treated as an on-off
+                                // flag
+
+                                bool hasAttributeDisabledFlag = currentStyle.Length >= 2 &&
+                                    int.TryParse(currentStyle.AsSpan(1, 1), out int attributeFlag) &&
+                                    attributeFlag == 0;
+                                Match capturedBoldDisabledTag = REGEX_B_TAG.Match(currentStyle);
+                                bool hasBoldDisabledFlag = capturedBoldDisabledTag.Success &&
+                                    capturedBoldDisabledTag.Groups.Count >= 2 &&
+                                    int.TryParse(capturedBoldDisabledTag.Groups[1].Value, out int boldDisabledFlag) &&
+                                    boldDisabledFlag < 500;
+                                if (hasAttributeDisabledFlag || hasBoldDisabledFlag)
+                                {
+                                    // close the tag
+
+                                    if (subTagsToClose.Contains(currentTag))
+                                    {
+                                        // nothing to be done if the tag hasn't
+                                        // been opened yet
+                                        // also tags must be nested, this also
+                                        // ensured the tag nested inside the tag
+                                        // being closed are also closed, then
+                                        // opened again once the current tag is
+                                        // closed
+
+                                        while (subTagsToClose.Count > 0)
+                                        {
+                                            string currentClosingTag = subTagsToClose.Pop();
+                                            styleReplacement.Append($"</{currentClosingTag}>");
+                                            if (!string.Equals(currentClosingTag, currentTag))
+                                            {
+                                                subTagsToOpen.Add(currentClosingTag);
+                                            }
+                                            else
+                                            {
+                                                // no need to close the tags
+                                                // that the current tag is
+                                                // nested within
+
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    // open the tag
+
+                                    if (!subTagsToClose.Contains(currentTag))
+                                    {
+                                        // nothing needs to be done if the tag
+                                        // is already open, else place the tag
+                                        // at the bottom of the stack of the
+                                        // tags being opened
+
+                                        subTagsToOpen.Insert(0, currentTag);
+                                    }
+                                }
+                            }
+                            else if (string.Equals(currentTag, "r"))
+                            {
+                                // reset override tags by closing all open tags
+                                // TODO: 'r' could also be used to switch styles
+
+                                while (subTagsToClose.Count > 0)
+                                {
+                                    styleReplacement.Append($"</{subTagsToClose.Pop()}>");
+                                }
+                            }
+                        }
+
+                        // insert open tags for tags in the to-open list
+
+                        for (int subTagsToOpenIndex = subTagsToOpen.Count - 1; subTagsToOpenIndex >= 0; subTagsToOpenIndex--)
+                        {
+                            string currentOpeningTag = subTagsToOpen[subTagsToOpenIndex];
+                            styleReplacement.Append($"<{currentOpeningTag}>");
+                            subTagsToClose.Push(currentOpeningTag);
+                        }
+                        subTagsToOpen.Clear();
+                    }
+                }
             }
 
-            // loop next tag
+            // replace override tags and loop next tag
 
-            lineText = REGEX_STYLE.Replace(lineText, styleReplacement);
+            lineText = REGEX_STYLE.Replace(lineText,
+                styleReplacement.ToString(),
+                1);
             capturedStyle = REGEX_STYLE.Match(lineText);
         }
 
@@ -181,6 +349,100 @@ public static class VttConverter
             sbRet.Append($"</{subTagsToClose.Pop()}>");
         }
         sbRet.Append("\r\n");
+
+        return sbRet.ToString();
+    }
+
+    /// <summary>
+    /// Converts ASS input stream to VTT through the output stream.
+    /// </summary>
+    /// <param name="input">Input ASS stream.</param>
+    /// <param name="output">Output stream.</param>
+    /// <param name="callback">
+    /// Callback to be invoked on each line processed.
+    /// </param>
+    /// <returns>Awaitable task for processing.</returns>
+    public static async Task ConvertStreamAsync(StreamReader input, StreamWriter output, OnLineOutputCallback? callback = null)
+    {
+        bool hasHeaderPrinted = false;
+        int currentVttLine = 1;
+
+        while (!input.EndOfStream)
+        {
+            string? currentLine = await input.ReadLineAsync();
+            string? convertedLine = ConvertLine(currentLine);
+
+            if (!string.IsNullOrWhiteSpace(convertedLine))
+            {
+                if (!hasHeaderPrinted)
+                {
+                    await output.WriteAsync("WEBVTT\r\n\r\n");
+                    await output.FlushAsync();
+
+                    hasHeaderPrinted = true;
+                }
+
+                await output.WriteAsync($"{currentVttLine}\r\n{convertedLine}");
+                if (!input.EndOfStream)
+                {
+                    await output.WriteAsync("\r\n");
+                }
+
+                await output.FlushAsync();
+
+                callback?.Invoke(convertedLine);
+
+                currentVttLine++;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Converts ASS string input with the specified encoding to VTT string.
+    /// This processes the input using MemoryStream, thus required to specify an
+    /// encoding.
+    /// </summary>
+    /// <param name="input">Input string.</param>
+    /// <param name="encoding">String encoding type.</param>
+    /// <param name="printHeader">Whether to output a WebVTT header.</param>
+    /// <param name="callback">
+    /// Callback to be invoked on each line processed.
+    /// </param>
+    /// <returns>
+    /// Awaitable task for processing, returning converted string.
+    /// </returns>
+    public static async Task<string> ConvertStringAsync(string input, Encoding encoding, bool printHeader = false, OnLineOutputCallback? callback = null)
+    {
+        bool hasHeaderPrinted = false;
+        int currentVttLine = 1;
+
+        MemoryStream inputStream = new MemoryStream(encoding.GetBytes(input));
+        StreamReader inputStreamReader = new StreamReader(inputStream);
+
+        StringBuilder sbRet = new StringBuilder();
+
+        while (!inputStreamReader.EndOfStream)
+        {
+            string? currentLine = await inputStreamReader.ReadLineAsync();
+            string? convertedLine = ConvertLine(currentLine);
+
+            if (!string.IsNullOrWhiteSpace(convertedLine))
+            {
+                if (!hasHeaderPrinted && printHeader)
+                {
+                    sbRet.Append("WEBVTT\r\n\r\n");
+                    hasHeaderPrinted = true;
+                }
+
+                sbRet.Append($"{currentVttLine++}\r\n{convertedLine}");
+                if (!inputStreamReader.EndOfStream)
+                {
+                    sbRet.Append("\r\n");
+                }
+
+                callback?.Invoke(convertedLine);
+            }
+        }
 
         return sbRet.ToString();
     }
--- Kyutorius.AstonishedVendetta.Tests/VttConverterTests.cs
+++ Kyutorius.AstonishedVendetta.Tests/VttConverterTests.cs
@@ -1,6 +1,9 @@
 // Copyright (c) shigeru22. Licensed under the MIT license.
 // See LICENSE in the repository root for details.
 
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Kyutorius.AstonishedVendetta.Foundation;
 
 namespace Kyutorius.AstonishedVendetta.Tests;
@@ -20,6 +23,24 @@ public class VttConverterTests
     private const string SINGLE_LINE_TEST_3 = "Dialogue: 0,0:00:02.00,0:00:05.00,,,0,0,0,,This is the first subtitle.";
     private const string SINGLE_LINE_RESULT_3 = "00:00:02.000 --> 00:00:05.000\r\nThis is the first subtitle.\r\n";
 
+    // single line with override styles
+    private const string SINGLE_LINE_STYLED_TEST_1 = @"Dialogue: 0,0:00:02.00,0:00:05.00,,,0,0,0,,This is the {\b1}first{\b0} subtitle.";
+    private const string SINGLE_LINE_STYLED_RESULT_1 = "00:00:02.000 --> 00:00:05.000\r\nThis is the <b>first</b> subtitle.\r\n";
+
+    // multiple lines 1 - without header
+    private const string MULTIPLE_LINE_TEST_1 = "Dialogue: 0,0:00:02.00,0:00:05.00,,,0,0,0,,This is the {\\b1}first{\\b0} subtitle.\r\nDialogue: 0,0:00:06.00,0:00:10.00,Bold,,0,0,0,,Here is a bold subtitle.\r\nDialogue: 0,0:00:12.00,0:00:16.00,Default,,0,0,0,,This is another {\\b1}Bold{\\b0} line of text.\r\n";
+    private const string MULTIPLE_LINE_RESULT_1 = "1\r\n00:00:02.000 --> 00:00:05.000\r\nThis is the <b>first</b> subtitle.\r\n\r\n2\r\n00:00:06.000 --> 00:00:10.000\r\n<v Bold>Here is a bold subtitle.\r\n\r\n3\r\n00:00:12.000 --> 00:00:16.000\r\n<v Default>This is another <b>Bold</b> line of text.\r\n";
+
+    // multiple lines 2 - without header
+    private const string MULTIPLE_LINE_TEST_2 = "Dialogue: 0,0:00:02.00,0:00:05.00,,,0,0,0,,This is the {\\b1}first{\\b0} subtitle.\r\nDialogue: 0,0:00:06.00,0:00:10.00,Bold,,0,0,0,,Here is a bold subtitle.\r\nDialogue: 0,0:00:12.00,0:00:16.00,Default,,0,0,0,,This is another {\\b1}Bold{\\b0} line of text.\r\n";
+    private const string MULTIPLE_LINE_RESULT_2 = "WEBVTT\r\n\r\n1\r\n00:00:02.000 --> 00:00:05.000\r\nThis is the <b>first</b> subtitle.\r\n\r\n2\r\n00:00:06.000 --> 00:00:10.000\r\n<v Bold>Here is a bold subtitle.\r\n\r\n3\r\n00:00:12.000 --> 00:00:16.000\r\n<v Default>This is another <b>Bold</b> line of text.\r\n";
+
+    // file processing
+    private const string FILE_TEST_EXPECTED_FILE_NAME = "0-expected.vtt";
+    private const string FILE_TEST_INPUT_FILE_NAME = "1-input.ass";
+    private const string FILE_TEST_OUTPUT_FILE_NAME = "2-output.vtt";
+
+
     [Fact(DisplayName = "Single line tests (simple)")]
     public void SingleLineTests()
     {
@@ -29,5 +50,55 @@ public class VttConverterTests
         Assert.Equal(SINGLE_LINE_RESULT_2, result2);
         string? result3 = VttConverter.ConvertLine(SINGLE_LINE_TEST_3);
         Assert.Equal(SINGLE_LINE_RESULT_3, result3);
+    }
+
+    [Fact(DisplayName = "Single line test (with override styles)")]
+    public void SingleLineTestWithOverrideStyles()
+    {
+        string? result1 = VttConverter.ConvertLine(SINGLE_LINE_STYLED_TEST_1);
+        Assert.Equal(SINGLE_LINE_STYLED_RESULT_1, result1);
+    }
+
+    [Fact(DisplayName = "Multiple line tests")]
+    public async Task MultipleLineTests()
+    {
+        string? result1 = await VttConverter.ConvertStringAsync(MULTIPLE_LINE_TEST_1, Encoding.UTF8);
+        Assert.Equal(MULTIPLE_LINE_RESULT_1, result1);
+        string? result2 = await VttConverter.ConvertStringAsync(MULTIPLE_LINE_TEST_2,
+            Encoding.UTF8,
+            true);
+        Assert.Equal(MULTIPLE_LINE_RESULT_2, result2);
+    }
+
+    [Fact(DisplayName = "File processing tests")]
+    public async Task FileProcessingTests()
+    {
+        char pathSeparator = Path.DirectorySeparatorChar;
+        string resourcesDirectory = $"{Directory.GetCurrentDirectory()}{pathSeparator}Resources";
+
+        string expectedFilePath = $"{resourcesDirectory}{pathSeparator}{FILE_TEST_EXPECTED_FILE_NAME}";
+        string inputFilePath = $"{resourcesDirectory}{pathSeparator}{FILE_TEST_INPUT_FILE_NAME}";
+        string outputFilePath = $"{resourcesDirectory}{pathSeparator}{FILE_TEST_OUTPUT_FILE_NAME}";
+
+        FileStream fsInput = new FileStream(inputFilePath, FileMode.Open);
+        FileStream fsOutput = new FileStream(outputFilePath,
+            FileMode.Create,
+            FileAccess.Write);
+
+        await VttConverter.ConvertStreamAsync(new StreamReader(fsInput), new StreamWriter(fsOutput));
+
+        fsInput.Close();
+        fsOutput.Close();
+
+        FileStream fsExpected = new FileStream(expectedFilePath, FileMode.Open);
+        fsOutput = new FileStream(outputFilePath, FileMode.Open);
+
+        string expected = await new StreamReader(fsExpected).ReadToEndAsync();
+        string result = await new StreamReader(fsOutput).ReadToEndAsync();
+
+        fsExpected.Close();
+        fsOutput.Close();
+
+        Assert.Equal(expected, result);
     }
 }
